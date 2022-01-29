22360939ed9f7b8a886222bbc003f634339cc1d2
John-Doe <proffessor7891@gmail.com>
2022-01-29 10:07:04 +0300

    ok

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -2,7 +2,89 @@
 function App() {
   return (
     <div className="wrapper">
-      gooo
+      <header>
+        <div className="header-left">
+          <img width={40} height={40} src="/img/zara1.png" />
+          <div className="header-info">
+            <h3>React Sneakers</h3>
+            <p>shoose magnaise</p>
+          </div>
+        </div>
+
+        <div className="header-right">
+        <ul>
+          <li>
+          <img width={18} height={18} src="/img/zara1.png" />
+            <span>1205 uah</span>
+          </li>
+          <li>
+          <img width={18} height={18} src="/img/zara1.png" />
+          </li>
+        </ul>
+        </div>
+      </header>
+      <div className="content">
+         <h1>all shooes</h1>
+         
+        <div className="shooses">
+          
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/1.jpg"/>
+          <p>mens shoes</p>
+          <div className="cardBottom">
+            <div className="cardPrice">
+              <span>price</span>
+              <b>999 uah</b>
+            </div>
+            <button>
+                <img width={11} height={11} src="/img/zara1.png" />
+            </button>
+          </div>
+        </div>  
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/1.jpg"/>
+          <p>mens shoes</p>
+          <div className="cardBottom">
+            <div className="cardPrice">
+              <span>price</span>
+              <b>999 uah</b>
+            </div>
+            <button>
+                <img width={11} height={11} src="/img/zara1.png" />
+            </button>
+          </div>
+        </div>  
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/1.jpg"/>
+          <p>mens shoes</p>
+          <div className="cardBottom">
+            <div className="cardPrice">
+              <span>price</span>
+              <b>999 uah</b>
+            </div>
+            <button>
+                <img width={11} height={11} src="/img/zara1.png" />
+            </button>
+          </div>
+        </div>  
+        <div className="card">
+          <img width={133} height={112} src="/img/sneakers/1.jpg"/>
+          <p>mens shoes</p>
+          <div className="cardBottom">
+            <div className="cardPrice">
+              <span>price</span>
+              <b>999 uah</b>
+            </div>
+            <button>
+                <img width={11} height={11} src="/img/zara1.png" />
+            </button>
+          </div>
+        </div>  
+          
+        </div>      
+
+
+      </div>
     </div>
   );
 }
