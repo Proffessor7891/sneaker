a37c3920b49f2881871af058baa37458af328e78
John-Doe <proffessor7891@gmail.com>
2022-01-30 16:03:11 +0300

    1

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -6,15 +6,119 @@ function App() {
      <div className="overlay">
       <div className="drawer">
          <h2>corsina</h2>
+         <button>c</button>
+      <div className="items">
 
         <div className="cartItem">
             <img width={70} height={70} src="/img/sneakers/1.jpg"/>
             <div>
               <p>mens shoose</p>
               <b>999 uah</b>
-            </div>
+            </div>     
             <button>0</button>
         </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+        <div className="cartItem">
+            <img width={70} height={70} src="/img/sneakers/1.jpg"/>
+            <div>
+              <p>mens shoose</p>
+              <b>999 uah</b>
+            </div>     
+            <button>0</button>
+        </div>
+      </div>
+        <ul>
+          <li>
+            <span>total</span>
+            <div></div>
+            <b>3400 uah</b>
+          </li>
+          <li>
+            <span>nds 5%</span>
+            <div></div>
+            <b>17 uah</b>
+          </li>
+        </ul>
+        <button>make order</button>
 
       </div>
     </div>
