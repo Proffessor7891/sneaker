c04eb131a78bd39d8d3e43cf3bc51e909dcb93ec
John-Doe <proffessor7891@gmail.com>
2022-02-02 15:40:33 +0300

    2

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -1,15 +1,55 @@
+import React from "react";
 import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
 
-const arr = [ 
-  {title: 'krytie tyfela', price: 560, imageUrl: "/img/sneakers/2.jpg" }, 
-  {title: 'nekrytie tyfela', price: 60, imageUrl: "/img/sneakers/3.jpg" }, 
-  {title: 'krytie sapogi', price: 5770, imageUrl: "/img/sneakers/4.jpg" },
-  {title: 'modnie tyfli', price: 320, imageUrl: "/img/sneakers/2.jpg" }, 
-]
+
 function App() {
+  const[products, setProducts] = React.useState([
+    {
+     "title": "krytie tyfela",
+     "price": "120",
+     "imageUrl": "/img/sneakers/2.jpg"
+    },
+    {
+     "title": "tyfela",
+     "price": "250",
+     "imageUrl": "/img/sneakers/3.jpg"
+    },
+    {
+     "title": "krytie ",
+     "price": "820",
+     "imageUrl": "/img/sneakers/4.jpg"
+    },
+    {
+     "title": "nekrytie tyfela",
+     "price": "330",
+     "imageUrl": "/img/sneakers/2.jpg"
+    },
+    {
+     "title": "krytie tyfela",
+     "price": "120",
+     "imageUrl": "/img/sneakers/2.jpg"
+    },
+    {
+     "title": "tyfela",
+     "price": "250",
+     "imageUrl": "/img/sneakers/3.jpg"
+    },
+    {
+     "title": "krytie ",
+     "price": "820",
+     "imageUrl": "/img/sneakers/4.jpg"
+    },
+    {
+     "title": "nekrytie tyfela",
+     "price": "330",
+     "imageUrl": "/img/sneakers/2.jpg"
+    }
+   ])
+
+
   return (
     <div className="wrapper">
      
@@ -25,7 +65,7 @@ function App() {
         <div className="shooses">
           
           
-          {arr.map((obj) => (
+          {products.map((obj) => (
             <Card
             title={obj.title}
             price={obj.price}
