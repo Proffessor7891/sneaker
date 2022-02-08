05c1a01cc1116aec3dad4ea8da8d1b87b95678ab
John-Doe <proffessor7891@gmail.com>
2022-02-08 21:18:55 +0300

    7

--- sneakers/src/App.js
+++ sneakers/src/App.js
@@ -6,9 +6,22 @@ import Header from "./components/Header";
 
 
 function App() {
+
+
   const [cartOpened, setCartOpened] = React.useState(false);
   const [products, setProducts] = React.useState([])
-  const [cartProducts, setCartProducts] = React.useState([])
+  const [cartProducts, setCartProducts] = React.useState([
+    {
+      "title": "krytie tyfela",
+      "price": "120",
+      "imageUrl": "/img/sneakers/2.jpg"
+     },
+     {
+      "title": "tyfela",
+      "price": "250",
+      "imageUrl": "/img/sneakers/3.jpg"
+     },
+  ])
 
   React.useEffect(() => {
   fetch('https://61fa3f3b31f9c200175966fa.mockapi.io/products')
@@ -23,7 +36,7 @@ function App() {
 
   return (
     <div className="wrapper">
-      {cartOpened && <Drawer onClose={() => setCartOpened(false)} /> }
+      {cartOpened && <Drawer products={cartProducts} onClose={() => setCartOpened(false)} /> }
       <Header onClickCart={() => setCartOpened(true)}  />
       <div className="content">
          <h1>all shooes</h1>
